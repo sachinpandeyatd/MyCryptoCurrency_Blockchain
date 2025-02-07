994e455b86179a5498951c0d916e6c9e2174ee35
Sachin Pandey <sachinpan@infinite.com>
2025-02-07 10:40:17 +0530

    Create TransactionOutput class

--- src/main/java/org/example/TransactionOutput.java
+++ src/main/java/org/example/TransactionOutput.java
@@ -1,5 +1,22 @@
 package org.example;
 
-public class TransactionOutput {
+import java.security.PublicKey;
 
+public class TransactionOutput {
+    private String id;
+    private PublicKey recipient;
+    private float value;
+    private String parentTransactionId;
+
+    public TransactionOutput(PublicKey recipient, float value, String parentTransactionId){
+        this.recipient = recipient;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        id = Helper.hashToSHA_256(Helper.getStringFromKey(recipient) +
+                value + parentTransactionId);
+    }
+
+    public boolean isMine(PublicKey publicKey){
+        return (publicKey == recipient);
+    }
 }
